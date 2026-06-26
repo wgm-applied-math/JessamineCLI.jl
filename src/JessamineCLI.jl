073fc039b968f2d5761bba68ee654be12385a178
W. Garrett Mitchener <mitchenerg@charleston.edu>
2026-06-26 14:00:42 -0400

    Bringing over from JessamineSciKitLearn

--- src/JessamineCLI.jl
+++ src/JessamineCLI.jl
@@ -1,5 +1,25 @@
 module JessamineCLI
 
-# Write your package code here.
+using Accessors
+using Dates
+using Accessors
+using Distributions
+using Printf
+using Random
+
+using JSON
+using StatsBase
+using StructUtils
+using Symbolics
+using SymbolicUtils
+using TermInterface
+
+using Jessamine
+using JessamineSymbolics
+
+include("CField.jl")
+include("Config.jl")
+include("Search.jl")
+include("Regression.jl")
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,10 @@
-using JessamineCLI
+using Dates
+using Random
 using Test
+using JessamineCLI
+
+Random.seed!(0x30c4070874d73da0)
 
 @testset "JessamineCLI.jl" begin
-    # Write your tests here.
+    include("TestRegression.jl")
 end
