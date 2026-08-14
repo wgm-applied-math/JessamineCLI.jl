f50eac74dd98dda2f8a7fc0a4718375ac70a47b5
W. Garrett Mitchener <garrett.mitchener@gmail.com>
2026-08-13 21:55:29 -0400

    Working on Documenter stuff

---
+++ docs/setup.jl
@@ -0,0 +1,5 @@
+using Pkg
+Pkg.develop(PackageSpec(url="https://github.com/wgm-applied-math/Jessamine.jl.git"))
+Pkg.develop(PackageSpec(url="https://github.com/wgm-applied-math/JessamineSymbolics.jl.git"))
+Pkg.develop(PackageSpec(path=pwd()))
+Pkg.instantiate()
